2cb958bf64dfe20656a7e919dce61067c6f7fb71
Pere Tarrida Domingo <58071471+peretarrida13@users.noreply.github.com>
2020-11-15 21:30:39 +0100

    Create styles.cc

---
+++ static/css/styles.cc
@@ -0,0 +1,46 @@
+.home-page{
+    background-color: rgb(153, 204, 255);
+}
+
+.game-title{
+    width: 75%;
+    margin: 0 auto;
+    text-align: center;
+    justify-items: center;
+    font-family: Arial;
+    font-size: large;
+    color: rgb(255, 255, 153);
+}
+
+.wait-txt, .final-title{
+    width: 75%;
+    margin: 10px auto;
+    text-align: center;
+    justify-items: center;
+    font-family: Arial;
+    color: rgb(255, 255, 153);
+}
+
+.main-btn{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.rock-paper-scissor-btn{
+    width: 75%;
+    margin: 0 auto;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+}
+
+.rock-paper-scissor-btn input{
+    justify-items: space-around;
+    margin: 25px 10px;
+    width: 105;
+    height: 105;
+    box-shadow: 10px 10px 5px gray;
+}
